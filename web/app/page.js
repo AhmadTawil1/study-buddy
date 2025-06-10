e2e82607fef67f8242a063b788cdb874f8d86f39
AhmadTawil1 <ahmadtawil.se@outlook.com>
2025-06-10 17:27:35 +0300

    cleanning

--- web/app/page.js
+++ web/app/page.js
@@ -4,13 +4,10 @@ import FeaturesSection from '../src/components/common/FeaturesSection'
 import HowItWorksSection from '../src/components/common/HowItWorksSection'
 import LivePreviewSection from '../src/components/common/LivePreviewSection'
 import CommunitySnapshot from '../src/components/common/CommunitySnapshot'
-import Footer from '../src/components/layout/Footer'
-import Navbar from '../src/components/layout/Navbar'
 
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-gray-50">
-      {/* <Navbar /> */}
       <main className="w-full">
         <HeroSection />
         <LivePreviewSection />
@@ -19,7 +16,6 @@ export default function HomePage() {
         <HowItWorksSection />
         <CommunitySnapshot />
       </main>
-      {/* <Footer /> */}
     </div>
   )
 }
